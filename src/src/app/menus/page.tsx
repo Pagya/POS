d3f52229128cb9f1fd73d1627e0217932980eae9
Pagya O <pagya.o@idp.com>
2026-04-22 12:37:54 +0530

    fix: remove nested src/src duplication

--- src/src/app/menus/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
-export default function MenusRedirect() {
-  const router = useRouter();
-  useEffect(() => { router.replace('/items'); }, [router]);
-  return null;
-}
